499a23ce2882b2d1618780952c5449c7e47f5232
Mavridiy <161286685+Mavridiy@users.noreply.github.com>
2024-08-05 17:00:38 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,8 @@
+name = 'Иван'
+print('Name: ' +name)
+age = 30
+print('Age: ' + str(age))
+age_new = age + 1
+print('New Age: ' + str(age_new))
+is_student = 1
+print('Is Student: ' + str(is_student == 1))
